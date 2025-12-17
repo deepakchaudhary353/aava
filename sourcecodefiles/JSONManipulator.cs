9801e51e39328e543ed93025c19084625596e9eb
Deepak Chaudhary <72210912+deepakchaudhary353@users.noreply.github.com>
2025-12-17 11:01:47 +0530

    Add sourcecodefiles/JSONManipulator.cs via automated upload

--- sourcecodefiles/JSONManipulator.cs
+++ sourcecodefiles/JSONManipulator.cs
@@ -1,4 +1,19 @@
-
 using System;
 using System.Collections.Generic;
-...<truncated for brevity>...
+using System.Text.Json;
+
+namespace sourcecodefiles
+{
+    public class JSONManipulator
+    {
+        public string SerializeDictionary(Dictionary<string, object> dict)
+        {
+            return JsonSerializer.Serialize(dict);
+        }
+
+        public Dictionary<string, object> DeserializeDictionary(string json)
+        {
+            return JsonSerializer.Deserialize<Dictionary<string, object>>(json);
+        }
+    }
+}
